6419c9fc2075b92f26709642f4599aa7fe634658
Marco <macl93@hotmail.com>
2018-06-08 13:51:12 -0500

    Agregar archivos de proyecto.

---
+++ SION/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SION
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SION/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SION.MvcApplication" Language="C#" %>
